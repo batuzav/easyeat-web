3eefefe6fc585f7d37608ebce84541e9f9d01ea7
Pedro Abraham Valdez Mejia <abrahamupsin@sitiorandom.com>
2018-11-05 09:58:08 -0600

    make it better

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ io.on('connection', function(socket) { //habla al metodo connection
                     "payment_method": "paypal"
                 },
                 "redirect_urls": {
-                    "return_url": "http://localhost:3000/process",
+                    "return_url": { status: 202 },
                     "cancel_url": "http://localhost:3000/cancel",
                 },
                 "transactions": [{
