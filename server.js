93b047e6a367f46a969d6e89ed275925b4da40fc
Pedro Abraham Valdez Mejia <abrahamupsin@sitiorandom.com>
2018-11-06 09:42:40 -0600

    make it better

--- server.js
+++ server.js
@@ -183,57 +183,73 @@ io.on('connection', function(socket) { //habla al metodo connection
         }
 
         if (msg.metodoPago === '  Paypal') {
-            var payReq = JSON.stringify({
-
+            const create_payment_json = {
                 "intent": "sale",
                 "payer": {
                     "payment_method": "paypal"
                 },
                 "redirect_urls": {
-                    "return_url": "https://www.paypal.com",
-                    "cancel_url": "https://www.paypal.com",
+                    "return_url": "https://easyeat-web.herokuapp.com/success",
+                    "cancel_url": "https://easyeat-web.herokuapp.com/cancel"
                 },
                 "transactions": [{
                     "item_list": {
                         "items": [{
-                            "name": "tacos",
+                            "name": "Red Sox Hat",
                             "sku": "001",
-                            "price": "20.00",
-                            "currency": "MXN",
+                            "price": "25.00",
+                            "currency": "USD",
                             "quantity": 1
                         }]
                     },
                     "amount": {
-                        "currency": "MXN",
-                        "total": "20.00"
+                        "currency": "USD",
+                        "total": "25.00"
                     },
-                    "description": "This is the payment description."
+                    "description": "Hat for the best team ever"
                 }]
-            });
+            };
 
-            paypal.payment.create(payReq, async function(error, payment) {
-                var links = {};
-
+            paypal.payment.create(create_payment_json, function(error, payment) {
                 if (error) {
-                    console.error(JSON.stringify(error));
+                    throw error;
                 } else {
-                    // Capture HATEOAS links
-                    console.log(payment)
-                    payment.links.forEach(async function(linkObj) {
-                        links[linkObj.rel] = await {
-                            href: linkObj.href,
-                            method: linkObj.method
-                        };
-                    })
-
-                    // If the redirect URL is present, redirect the customer to that URL
-                    if (links.hasOwnProperty('approval_url')) {
-                        // Redirect the customer to links['approval_url'].href
-                    } else {
-                        console.error('no redirect URI present');
+                    for (let i = 0; i < payment.links.length; i++) {
+                        if (payment.links[i].rel === 'approval_url') {
+                            res.redirect(payment.links[i].href);
+                        }
                     }
                 }
             });
+
+
+
+            app.get('/success', (req, res) => {
+                const payerId = req.query.PayerID;
+                const paymentId = req.query.paymentId;
+
+                const execute_payment_json = {
+                    "payer_id": payerId,
+                    "transactions": [{
+                        "amount": {
+                            "currency": "USD",
+                            "total": "25.00"
+                        }
+                    }]
+                };
+
+                paypal.payment.execute(paymentId, execute_payment_json, function(error, payment) {
+                    if (error) {
+                        console.log(error.response);
+                        throw error;
+                    } else {
+                        console.log(JSON.stringify(payment));
+                        res.send('Success');
+                    }
+                });
+            });
+
+            app.get('/cancel', (req, res) => res.send('Cancelled'));
         }
 
 
