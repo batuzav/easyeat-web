b1b4e68a4653d74cc6aa33168d577bf6bdbff23f
Pedro Abraham Valdez Mejia <abrahamupsin@sitiorandom.com>
2018-11-23 12:29:25 -0600

    make it better

--- server.js
+++ server.js
@@ -114,6 +114,7 @@ app.get("/paypal", async(req, res) => {
 
         });
         console.log('esta es data: ', data);
+        console.log('Este es total de suma: ', total);
         totalventa = total;
         console.log('Este es el string de total: ', totalventa);
         console.log('Contador ' + cont + ' total: ' + total);
